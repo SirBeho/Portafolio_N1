efa57180298696e30c62e36a03634d86ce7faf94
SirBeho <123344546+SirBeho@users.noreply.github.com>
2023-07-29 16:32:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ const divPrueba = document.querySelector(".projects-grid");
       divPrueba.innerHTML = "";
       enlaces.forEach((enlace) => {
           let href = enlace.getAttribute('href').toLowerCase();
-      
+            console.log(href)
+
         if (href.length > 16 && href.includes(tipo)) {
           
            if(tipo == ''  && (href.includes('tarea') || href.includes('ejercicio')))
